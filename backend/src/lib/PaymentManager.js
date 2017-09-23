0188be2c0d9931fa0177efa90ec6e2f6509362bd
Sieu Tsoi <sieut@umich.edu>
2017-09-23 19:05:08 -0400

    Transfer to fund endpoint

--- backend/src/lib/PaymentManager.js
+++ backend/src/lib/PaymentManager.js
@@ -3,24 +3,28 @@ import CapitalOneApiManager from "./CapitalOneApiManager.js"
 import UserManager from "./UserManager.js"
 import GroupManager from "./GroupManager.js"
 
-
 class _PaymentManager {
 
     constructor() {
 
     }
 
-    makePaymentToFund(Group_id, Fund_id, Amount, User_id){
-        let user = UserManager.GetUserInfo(User_id);
-        let group = GroupManager.GetGroupInfo(Group_id);
-        
+    payToFund(group, fund, user, amount){
+        return CapitalOneApiManager.CreateTransfer(group.account_id, user.account_number, amount, "PAID TO " + fund.fund_name);
 
-        let newTransfer = await CapitalOneApiManager.CreateTransfer(group.account_id ,user.UserAccountNumber, Amount, "PAID TO " + user.name);
-        Database.query("SELECT ")
+        let user_id = request.body.user_id;
+        let fund_id = request.body.fund_id;
+        let amount = request.body.amount;
 
+        UserManager.GetUserInfo(user_id).then(user => {
+            CapitalOneApiManager.CreateTransfer(group.account_id ,user.UserAccountNumber, Amount, "PAID TO " + user.name).then(result => {
+                FundManager.updateFundAmount(fund_id, amount).then(result => {
+                    response.status(200).message({ message: "Fund transfered" });
+                });
+            });
+        });
     }
 }
-
 
 const FundManager = new _FundManager();
 export default FundManager;
