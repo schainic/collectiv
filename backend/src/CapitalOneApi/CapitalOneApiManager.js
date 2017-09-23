6619afef8d825b580f8b54efd936ca5a289eceeb
Ben Wireman <bwireman1@gmail.com>
2017-09-23 11:49:31 -0400

    more interact code for posting CApitalone models

--- backend/src/CapitalOneApi/CapitalOneApiManager.js
+++ backend/src/CapitalOneApi/CapitalOneApiManager.js
@@ -1,3 +1,9 @@
+
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+import Config from 'config/Config.js';
+
 class CapitalOneApiManager {
     constructor() {
 
@@ -6,7 +12,7 @@ class CapitalOneApiManager {
 
     CreateCustomer(FirstName, LastName, StreeNumber, StreetName, City, State, Zip)
     {
-        let newcustomer = {
+        let newCustomer = querystring.stringify({
             "first_name": FirstName,
             "last_name": LastName,
             "address": {
@@ -16,11 +22,131 @@ class CapitalOneApiManager {
                 "state": State,
                 "zip": Zip
             }
+        });
+
+        let key;
+
+        try{
+            key = await Config.CapitalOne.ApiKey;
+        }catch(e) {
+            console.log("Failed to retreive APIKEY for Capital one!!! in createcustomrer() lol shit motherfucker!");
         }
 
 
+        let post_options = {
+            host: 'api.reimaginebanking.com',
+            path: '/customers',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(newCustomer)
+            },
+            parameters: {
+                'key': key
+            }
+        };
 
+        let post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
 
+        // post the data
+        post_req.write(newCustomer);
+        post_req.end();
     }
+
+    CreateAccount(CustomerId, NickName)
+    {
+        let newAccount = {
+            "type": "Savings",
+            "nickname": "test account",
+            "rewards": 0,
+            "balance": 0
+        };
+
+        let key;
+
+        try{
+            key = await Config.CapitalOne.ApiKey;
+        }catch(e) {
+            console.log("Failed to retreive APIKEY for Capital one!!! in createcustomrer() lol shit motherfucker!");
+        }
+
+
+        let post_options = {
+            host: 'api.reimaginebanking.com',
+            path: '/customers/' + CustomerId + '/accounts' ,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(newCustomer)
+            },
+            parameters: {
+                'key': key
+            }
+        };
+
+        let post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
+
+        // post the data
+        post_req.write(newAccount);
+        post_req.end();
+    }
+
+    CreateTransfer(PayerId, PayeeId, amount, Description)
+    {
+        let today = new Date();
+
+
+        let newTransfer = {
+            "medium": "balance",
+            "payee_id": PayeeId,
+            "amount": amount,
+            "transaction_date": today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
+            "description": Description
+        }
+
+        let key;
+
+        try{
+            key = await Config.CapitalOne.ApiKey;
+        }catch(e) {
+            console.log("Failed to retreive APIKEY for Capital one!!! in createcustomrer() lol shit motherfucker!");
+        }
+
+
+        let post_options = {
+            host: 'api.reimaginebanking.com',
+            path: '/accounts/' + PayerId + '/transfers' ,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(newCustomer)
+            },
+            parameters: {
+                'key': key
+            }
+        };
+
+        let post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
+
+        // post the data
+        post_req.write(newTransfer);
+        post_req.end();
+    }
+
 
 }
