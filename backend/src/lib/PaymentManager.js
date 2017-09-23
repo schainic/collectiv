747a4a6400425e0a13613228f77aca954c5da45e
Sieu Tsoi <sieut@umich.edu>
2017-09-23 19:21:20 -0400

    Fuck

--- backend/src/lib/PaymentManager.js
+++ backend/src/lib/PaymentManager.js
@@ -10,18 +10,6 @@ class _PaymentManager {
 
     payToFund(group, fund, user, amount){
         return CapitalOneApiManager.CreateTransfer(group.account_id, user.account_number, amount, "PAID TO " + fund.fund_name);
-
-        let user_id = request.body.user_id;
-        let fund_id = request.body.fund_id;
-        let amount = request.body.amount;
-
-        UserManager.GetUserInfo(user_id).then(user => {
-            CapitalOneApiManager.CreateTransfer(group.account_id ,user.UserAccountNumber, Amount, "PAID TO " + user.name).then(result => {
-                FundManager.updateFundAmount(fund_id, amount).then(result => {
-                    response.status(200).message({ message: "Fund transfered" });
-                });
-            });
-        });
     }
 }
 
