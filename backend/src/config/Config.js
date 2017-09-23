1d67b893f0a99d10059d6ab8f53f4c4665c01a47
Sieu Tsoi <sieut@umich.edu>
2017-09-23 11:07:12 -0400

    Add Config imple

--- backend/src/config/Config.js
+++ backend/src/config/Config.js
@@ -1,5 +1,26 @@
+const path = require("path");
 const fs = require("fs");
 
-export default class Config {
+import cloneDeep from "lodash/cloneDeep"
 
+const CONF_PATH = path.join(__dirname, "Config.json");
+
+class _Config {
+    constructor() {
+        let config = JSON.parse(fs.readFileSync(CONF_PATH));
+
+        this._capitalOne = config.CapitalOne;
+        this._postgres = config.Postgres;
+    }
+
+    get capitalOne() {
+        return cloneDeep(this._capitalOne);
+    }
+
+    get postgres() {
+        return cloneDeep(this._postgres);
+    }
 }
+
+const Config = new _Config();
+export default Config;
