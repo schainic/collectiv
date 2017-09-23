313b9fbfb53196a74248f69aeb9a409fbfee64af
Sieu Tsoi <sieut@umich.edu>
2017-09-23 12:00:45 -0400

    Server init for session and passport

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -17,7 +17,24 @@ class _Server {
     }
 
     _initServer() {
+		// Use cookie-parser
+		this._server.use(cookieParser());
 
+		// Use body-parser
+		this._server.use(bodyParser.json());
+		this._server.use(bodyParser.urlencoded({ extended:true }));
+
+		// Use session
+		let sessionConfig = {
+			secret: ServerConfig.sessionSecret,
+			resave: false,
+			saveUninitialized: true
+		}
+		this._server.use(session(sessionConfig));
+
+		// Use passport
+		this._server.use(passport.initialize());
+		this._server.use(passport.session());
     }
 
     _initRoutes() {
